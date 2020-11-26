82a74f869fb7904f356c782ded881a63c6813aa1
Leonardo <leonardoporto51@gmail.com>
2020-11-26 17:58:01 -0300

    Dependencias e configurações do banco

---
+++ config/dbconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: '*****',
+    password: '*****',
+    database: 'fifo',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }   
+}
+
